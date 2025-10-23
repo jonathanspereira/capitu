6166502c54bb462526cff20f31ec66d7db137503
jonathanspereira <jonathanpereira.jsp@outlook.com>
2025-10-22 21:08:49 -0300

    build(deps):  installation of nodejs, typescript, prisma, vitest, supertest

---
+++ src/routes/routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+
+class Routes {
+    static define(router: Router): Router {
+
+    
+
+        return router;
+    }
+}
+
+export default Routes.define(Router());
