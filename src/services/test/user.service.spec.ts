c9f4b8b5d5b38b65737c27c099f33f3f8803c2fd
jonathanspereira <jonathanpereira.jsp@outlook.com>
2025-10-24 11:28:11 -0300

    test(service): Fix UserService getProfile test.

--- src/services/test/user.service.spec.ts
+++ src/services/test/user.service.spec.ts
@@ -1,46 +1,38 @@
-import { test, expect, vi } from 'vitest'
-import { getProfile } from '../../services/user.service'
-import { UserDto } from '../../view/dto/users.dto'
-import prisma from '../../libs/prisma.lib';
+import { test, expect, vi } from 'vitest';
+import { getProfile } from '../../services/user.service';
+import { UserDto } from '../../view/dto/users.dto';
+import prisma from '../../libs/__mocks__/prisma.lib';
 
-vi.mock('../../libs/prisma.lib', () => ({
-  default: {
-    users: {
-      findUnique: vi.fn(),
-    },
-  },
-}));
-
+vi.mock('../../libs/prisma.lib');
 
 test('deve retornar o perfil do usuário quando existir', async () => {
+  const mockUser = {
+    id: 1,
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    password: 'hashed_password',
+    resetToken: 'any_token',
+    resetTokenExpiry: new Date(),
+  };
 
-    const mockUser = {
-      id: 1,
-      name: 'John Doe',
-      email: 'john.doe@example.com',
-      password: 'hashed_password',
-      resetToken: 'any_token',
-      resetTokenExpiry: new Date(),
-    };
+  prisma.users.findUnique.mockResolvedValue(mockUser);
 
-    (prisma.users.findUnique as any).mockResolvedValue(mockUser);
+  const result = await getProfile(1);
 
-    const result = await getProfile(1);
-
-    expect(result).toEqual(UserDto.fromEntity(mockUser));
-    expect(prisma.users.findUnique).toHaveBeenCalledWith({ where: { id: 1 } });
+  expect(result).toEqual(UserDto.fromEntity(mockUser));
+  expect(prisma.users.findUnique).toHaveBeenCalledWith({ where: { id: 1 } });
 });
 
 test('deve lançar um erro quando o usuário não for encontrado', async () => {
-    (prisma.users.findUnique as any).mockResolvedValue(null);
+  prisma.users.findUnique.mockResolvedValue(null);
 
-    await expect(getProfile(999)).rejects.toThrow('Usuário não encontrado.');
-    expect(prisma.users.findUnique).toHaveBeenCalledWith({ where: { id: 999 } });
+  await expect(getProfile(999)).rejects.toThrow('Usuário não encontrado.');
+  expect(prisma.users.findUnique).toHaveBeenCalledWith({ where: { id: 999 } });
 });
 
 test('deve lançar um erro quando ocorrer um problema no banco de dados', async () => {
-    (prisma.users.findUnique as any).mockRejectedValue(new Error('Database error'));
+  prisma.users.findUnique.mockRejectedValue(new Error('Database error'));
 
-    await expect(getProfile(1)).rejects.toThrow('Database error');
-    expect(prisma.users.findUnique).toHaveBeenCalledWith({ where: { id: 1 } });
+  await expect(getProfile(1)).rejects.toThrow('Database error');
+  expect(prisma.users.findUnique).toHaveBeenCalledWith({ where: { id: 1 } });
 });
