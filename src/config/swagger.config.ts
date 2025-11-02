187ccccfc37b692238afc269c73e19f106d03f3c
jonathanspereira <jonathanpereira.jsp@outlook.com>
2025-11-02 15:25:20 -0300

    fix: update build script to copy documentation and correct swagger file path

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 import { Express } from 'express';
 
 export const setupSwagger = (app: Express) => {
-  const swaggerFile = path.join(process.cwd(), 'src/docs/swagger.json');
+  const swaggerFile = path.join(__dirname, '../docs/swagger.json');
   const swaggerData = JSON.parse(fs.readFileSync(swaggerFile, 'utf8'));
 
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerData));
