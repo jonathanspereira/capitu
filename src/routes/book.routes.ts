f65e52aeb2d8d640d1346d15237f4da40e4e69af
jonathanspereira <jonathanpereira.jsp@outlook.com>
2025-11-06 11:45:23 -0300

    fix: update delete book route to use root path for consistency

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -9,7 +9,7 @@ bookRoutes.post("/", (req, res) => controller.addBook(req, res));
 bookRoutes.patch("/status", (req, res) => controller.updateStatus(req, res));
 bookRoutes.patch("/favorite", (req, res) => controller.toggleFavorite(req, res));
 bookRoutes.get("/user/:userId", (req, res) => controller.listUserBooks(req, res));
-bookRoutes.delete("/books", (req, res) => controller.removeBookFromUser(req, res));
+bookRoutes.delete("/", (req, res) => controller.removeBookFromUser(req, res));
 
 
 export default bookRoutes;
